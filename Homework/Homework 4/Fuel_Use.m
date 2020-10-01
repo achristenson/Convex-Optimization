7c8a0542451279cc6f3c8ff7e3615d1f357aa0b0
Alex Christenson <achristenson95@gmail.com>
2020-10-01 18:38:42 -0400

    Removing sensitive files

--- Homework/Homework 4/Fuel_Use.m
+++
@@ -1,5 +0,0 @@
-function x = Fuel_Use(a,b,c,s)
-
-x = a*s^2 + b*s + c;
-
-return
--- Homework/Homework 4/Problem10.m
+++
@@ -1,7 +0,0 @@
-% Code for Homework 4, Problem 10, Convex Optimization SP2019
-% Professor Mai Vu
-% 
-% Note to the user: make sure to run the veh_speed_sched_data.m script
-% before executing this file.
-
-f = @(x)
